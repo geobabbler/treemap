1d7f97dc8677363fb2dd35ad47f2876149421eb6
Chris Hogan <chris@hogan.io>
2015-08-26 19:53:53 -0400

    cleaning up the filter box, adding counts to trees, various code clean up

--- client/js/map/map.js
+++ client/js/map/map.js
@@ -3,96 +3,64 @@ var mapStuff = angular.module('mapStuff', ['ngRoute']);
 mapStuff.controller('mapController', ['$scope', '$rootScope', 'treeData',
   function($scope, $rootScope, treeData) {
 
-		/*
+    /*
      * Defaults
-    */
+     */
 
     //Set default state for treeLayer to be turned off
     $scope.neighborhoodLayerVisible = false;
 
     // default map starts for this project TODO: seperate starts into config file
     $scope.startX = 39.290452,
-    $scope.startY = -76.614090,
-    $scope.startZ = 16;
+      $scope.startY = -76.614090,
+      $scope.startZ = 16;
     $scope.maxBounds = L.latLngBounds(L.latLng(39.182989, -76.732924), L.latLng(39.408827, -76.500495));
 
-		/*
+    /*
      * Mapping / Leaflet parts
-    */
+     */
 
-		// create map object, set view to be the default map starts
-		$scope.map = L.map('map', {'zoomControl': false, maxBounds: $scope.maxBounds, minZoom: 12});
+    // create map object, set view to be the default map starts
+    $scope.map = L.map('map', {
+      'zoomControl': false,
+      maxBounds: $scope.maxBounds,
+      minZoom: 12
+    });
 
-		/*
+    /*
      * Layers
-    */
-		// two basemaps being included TODO: put some of these basemap options into config file, maybe array for basemap options
+     */
+    // two basemaps being included TODO: put some of these basemap options into config file, maybe array for basemap options
     $scope.basemap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       'maxZoom': 18,
-      'attribution': '© mapbox',
+      'attribution': 'mapbox/OSM',
       'id': 'mapbox.outdoors',
       'accessToken': 'pk.eyJ1IjoiaG9nYW5tYXBzIiwiYSI6ImFhMDUxZWFkNDhkZjkzMTU3MmYwNjJhN2VjYmFhN2U4In0.89HZWfBDYycfNzm1LIq3LA'
     }).addTo($scope.map);
 
     $scope.satellite = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       'maxZoom': 18,
-      'attribution': '© mapbox',
+      'attribution': 'mapbox/OSM',
       'id': 'mapbox.satellite',
       'accessToken': 'pk.eyJ1IjoiaG9nYW5tYXBzIiwiYSI6ImFhMDUxZWFkNDhkZjkzMTU3MmYwNjJhN2VjYmFhN2U4In0.89HZWfBDYycfNzm1LIq3LA'
     });
-
-		$scope.treeLayer = L.geoJson('', {
-			style: function(feature) {
-				switch (feature.properties.year) {
-					case 2011: return { fillColor: "#feb24c"};
-					case 2012: return { fillColor: "#fd8d3c"};
-					case 2013: return { fillColor: "#fc4e2a"};
-					case 2014: return { fillColor: "#e31a1c"};
-					case 2015: return { fillColor: "#b10026"};
-					default: return { fillColor: "#feb24c"};
-				}
-			},
-			pointToLayer: function(feature, latlng) {
-				return L.circleMarker(latlng, {
-					radius: 8,
-					color: 'white',
-					weight: .5,
-					opacity: 1,
-					fillOpacity: 0.65
-				})
-			}
-		});
-
 
     /*
       this empty unstyled geojson feeds the clusters
     */
     $scope.clusteredGeoJSON = L.geoJson('');
 
-    function highlightFeature(feature, layer) {
-        console.log(feature)
-    }
-
-    function resetHighlight(e) {
-        //stuff?
-        console.log(e);
-    }
-
-    $scope.onEachFeature = function(feature, layer) {
-        layer.on({
-            mouseover: highlightFeature(feature, layer),
-            mouseout: resetHighlight
-        });
-    }
-    $scope.neighborhoodsLayer = L.geoJson('', {onEachFeature: $scope.onEachFeature}).addTo($scope.map);
+    $scope.neighborhoodsLayer = L.geoJson('', {
+      onEachFeature: $scope.onEachFeature
+    }).addTo($scope.map);
 
     /*
-    *	this section is for map events
-    */
+     *	this section is for map events
+     */
 
-		//when the user clicks the icon that toggles the basemap
+    //when the user clicks the icon that toggles the basemap
     $rootScope.$on('baseLayerChange', function(e, d) {
-			// TODO build off array of basemaps in config maybe?
+      // TODO build off array of basemaps in config maybe?
       if (d === 'road') {
         $scope.basemap.addTo($scope.map);
         $scope.map.removeLayer($scope.satellite);
@@ -102,7 +70,7 @@ mapStuff.controller('mapController', ['$scope', '$rootScope', 'treeData',
       }
     });
 
-		//when the user clicks the zoom in and zoom out buttons
+    //when the user clicks the zoom in and zoom out buttons
     $rootScope.$on('zoomChange', function(e, d) {
       if (d === 'zoomIn') {
         $scope.map.zoomIn();
@@ -110,63 +78,157 @@ mapStuff.controller('mapController', ['$scope', '$rootScope', 'treeData',
       $scope.map.zoomOut();
     });
 
-		/*
-		not sure where to put this yet
-		*/
+    /*
+    Tree Layer Control
+    */
 
-		$scope.drawTrees = function(){
-      if($scope.map.getZoom() >= 16){
-  			treeData.showTrees({bbox: $scope.map.getBounds()}).then(function(data) {
+    $scope.treeConfig = {
+      "other": {
+        "label": "Before 2010",
+        "color": "#feb24c",
+        "count": 0
+      },
+      "2010": {
+        "label": "2010",
+        "color": "#fd8d3c",
+        "count": 0
+      },
+      "2011": {
+        "label": "2011",
+        "color": "#fc4e2a",
+        "count": 0
+      },
+      "2012": {
+        "label": "2012",
+        "color": "#e31a1c",
+        "count": 0
+      },
+      "2013": {
+        "label": "2013",
+        "color": "#b10026",
+        "count": 0
+      }
+    };
+
+    //empty layer for the trees
+    $scope.treeLayer = L.geoJson('', {
+      style: function(feature) {
+        switch (String(feature.properties.year)) {
+          case "2013":
+            return {
+              fillColor: $scope.treeConfig["2013"].color
+            };
+          case "2012":
+            return {
+              fillColor: $scope.treeConfig["2012"].color
+            };
+          case "2011":
+            return {
+              fillColor: $scope.treeConfig["2011"].color
+            };
+          case "2010":
+            return {
+              fillColor: $scope.treeConfig["2010"].color
+            };
+          default:
+            return {
+              fillColor: $scope.treeConfig["other"].color
+            };
+        }
+      },
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, {
+          radius: 8,
+          color: 'white',
+          weight: .5,
+          opacity: 1,
+          fillOpacity: 0.65
+        })
+      }
+    });
+
+    //function to draw the trees
+    $scope.drawTrees = function() {
+
+      if ($scope.map.getZoom() >= 16) {
+
+        treeData.showTrees({
+          bbox: $scope.map.getBounds(),
+          filter: $scope.disabledYearArray
+        }).then(function(data) {
 
           $scope.showClusterByReducedPrecisionLayer.clearLayers();
-  				$rootScope.$broadcast('treeCount', data.features.length);
+          $rootScope.$broadcast('treeCount', data.features.length);
           try {
             $scope.map.removeLayer($scope.markers);
-          }
-          catch(err){
+          } catch (err) {
             //
           }
-          $scope.treeLayer.clearLayers();
-  				$scope.treeLayer.addData(data);
-  				$scope.treeLayer.addTo($scope.map);
 
-  			}, function(err) {
-  				//failure!
-  			});
+          for (var item in $scope.treeConfig) {
+            $scope.treeConfig[item].count = 0;
+          };
+          for (var key in data.features) {
+
+            if ($scope.treeConfig[data.features[key].properties.year]) {
+              $scope.treeConfig[data.features[key].properties.year].count += 1;
+            }
+            if (!$scope.treeConfig[data.features[key].properties.year]) {
+              $scope.treeConfig["other"].count += 1;
+            }
+          }
+
+          $scope.treeLayer.clearLayers();
+          $scope.treeLayer.addData(data);
+          $scope.treeLayer.addTo($scope.map);
+
+        }, function(err) {
+          //failure!
+        });
       }
-      if(($scope.map.getZoom() > 14 )&&($scope.map.getZoom() < 16)) {
-        treeData.clusterByReducedPrecision({precision: 4, bbox: $scope.map.getBounds()}).then(function(data) {
+      if (($scope.map.getZoom() > 14) && ($scope.map.getZoom() < 16)) {
+        treeData.clusterByReducedPrecision({
+          precision: 4,
+          bbox: $scope.map.getBounds(),
+          filter: $scope.disabledYearArray
+        }).then(function(data) {
           $scope.treeLayer.clearLayers();
           $rootScope.$broadcast('treeCount', data.total);
 
           try {
             $scope.map.removeLayer($scope.markers);
-          }
-          catch(err){
+          } catch (err) {
             //
           }
 
-          $scope.markers = new L.MarkerClusterGroup({showCoverageOnHover: false});
+          $scope.markers = new L.MarkerClusterGroup({
+            showCoverageOnHover: false
+          });
           $scope.clusteredGeoJSON.addData(data);
 
           $scope.markers.addLayer($scope.clusteredGeoJSON);
           $scope.map.addLayer($scope.markers);
-  			}, function(err) {
-  				//failure!
-  			});
+        }, function(err) {
+          //failure!
+        });
       }
-      if($scope.map.getZoom() < 15) {
-        treeData.clusterByReducedPrecision({precision: 3, bbox: $scope.map.getBounds()}).then(function(data) {
+      if ($scope.map.getZoom() < 15) {
+        treeData.clusterByReducedPrecision({
+          precision: 3,
+          bbox: $scope.map.getBounds(),
+          filter: $scope.disabledYearArray
+        }).then(function(data) {
           $scope.treeLayer.clearLayers();
           $rootScope.$broadcast('treeCount', data.total);
           try {
             $scope.map.removeLayer($scope.markers);
-          }
-          catch(err){
+          } catch (err) {
             //
           }
 
-          $scope.markers = new L.MarkerClusterGroup({showCoverageOnHover: false});
+          $scope.markers = new L.MarkerClusterGroup({
+            showCoverageOnHover: false
+          });
           $scope.clusteredGeoJSON.addData(data);
 
           $scope.markers.addLayer($scope.clusteredGeoJSON);
@@ -176,68 +238,90 @@ mapStuff.controller('mapController', ['$scope', '$rootScope', 'treeData',
         });
       }
 
-		};
+    };
 
     $scope.neighborhoods = function() {
       //neighborhood button clicked
-      if($scope.neighborhoodLayerVisible){
-        treeData.showNeighborhoods({bbox: $scope.map.getBounds()}).then(function(data) {
+      if ($scope.neighborhoodLayerVisible) {
+        treeData.showNeighborhoods({
+          bbox: $scope.map.getBounds()
+        }).then(function(data) {
           $scope.neighborhoodsLayer.clearLayers();
           $scope.neighborhoodsLayer.addData(data);
           $scope.neighborhoodsLayer.bringToBack();
-  			}, function(err) {
-  				console.log(err);
-  				//failure!
-  			});
-      }
-      else {
+        }, function(err) {
+          console.log(err);
+          //failure!
+        });
+      } else {
         //
         $scope.neighborhoodsLayer.clearLayers();
       }
     }
-    $scope.toggleNeighborhoods = function(){
-        $scope.neighborhoodLayerVisible = !$scope.neighborhoodLayerVisible;
-        $scope.neighborhoods();
+    $scope.toggleNeighborhoods = function() {
+      $scope.neighborhoodLayerVisible = !$scope.neighborhoodLayerVisible;
+      $scope.neighborhoods();
+    }
+
+    //empty array to push hidden tree years in to
+    $scope.disabledYearArray = [];
+
+    //helper function that we will use to simplify pushing in to and out of an
+    // array, specifically for the trees
+    var indexOf = function(needle) {
+      if (typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+      } else {
+        indexOf = function(needle) {
+          var i = -1,
+            index = -1;
+
+          for (i = 0; i < this.length; i++) {
+            if (this[i] === needle) {
+              index = i;
+              break;
+            }
+          }
+
+          return index;
+        };
+      }
+
+      return indexOf.call(this, needle);
+    };
+
+    //push and splice tree year labels in and out of the array
+    $scope.toggleTreeYear = function(year) {
+      if (indexOf.call($scope.disabledYearArray, year) === -1) {
+        $scope.disabledYearArray.push(year);
+      } else {
+        $scope.disabledYearArray.splice(indexOf.call($scope.disabledYearArray, year), 1);
+      }
+      console.log($scope.disabledYearArray);
     }
 
     function getSize(d) {
       return d >= 40 ? 20 :
-             d >= 21 ? 15 :
-             d >= 6 ? 10 :
-             d >= 1 ? 4 :
-                        0;
+        d >= 21 ? 15 :
+        d >= 6 ? 10 :
+        d >= 1 ? 4 :
+        0;
     }
 
-    $scope.showClusterByReducedPrecisionLayer = L.geoJson('',{
-      pointToLayer: function (feature, latlng) {
-          return L.circleMarker(latlng, {
-            radius: getSize(feature.properties.count),
-            fillColor: "#ff7800",
-            color: "#000",
-            weight: 1,
-            opacity: 1,
-            fillOpacity: 0.8
-          }).addTo($scope.map);
-        }
-      }).addTo($scope.map);
+    $scope.showClusterByReducedPrecisionLayer = L.geoJson('', {
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, {
+          radius: getSize(feature.properties.count),
+          fillColor: "#ff7800",
+          color: "#000",
+          weight: 1,
+          opacity: 1,
+          fillOpacity: 0.8
+        }).addTo($scope.map);
+      }
+    }).addTo($scope.map);
     $scope.showClusterByReducedPrecision = false;
 
-    // $scope.toggleClusterByReducedPrecision = function(){
-    //   $scope.showClusterByReducedPrecision = !$scope.showClusterByReducedPrecision;
-    //   if($scope.showClusterByReducedPrecision == true){
-    //     var markers = L.markerClusterGroup();
-    //     treeData.clusterByReducedPrecision().then(function(data) {
-    //       $scope.showClusterByReducedPrecisionLayer.clearLayers();
-    //       $scope.showClusterByReducedPrecisionLayer.addData(data);
-    //       $scope.showClusterByReducedPrecisionLayer.bringToBack();
-  	// 		}, function(err) {
-  	// 			//failure!
-  	// 		});
-    //   }
-    //   else {
-    //     $scope.showClusterByReducedPrecisionLayer.clearLayers();
-    //   }
-    // }
 
     $scope.map.on('moveend', $scope.drawTrees);
     $scope.map.on('load', $scope.drawTrees);
--- server/app/controllers/geographic.controller.js
+++ server/app/controllers/geographic.controller.js
@@ -8,21 +8,21 @@ homicides CSV:  https://data.baltimorecity.gov/Public-Safety/Homicides/9h5s-7d88
 var pg = require('pg');
 
 var env = require('../../config/env.js'),
-    environment = new env(),
-    connstring = environment.connstring;
+  environment = new env(),
+  connstring = environment.connstring;
 
 /*
 this makes geojson output possible
 */
-function FeatureCollection(){
-    this.type = 'FeatureCollection';
-    this.features = new Array();
+function FeatureCollection() {
+  this.type = 'FeatureCollection';
+  this.features = new Array();
 }
 
-function Feature(){
-    this.type = 'Feature';
-    this.geometry = new Object;
-    this.properties = new Object;
+function Feature() {
+  this.type = 'Feature';
+  this.geometry = new Object;
+  this.properties = new Object;
 }
 
 /*
@@ -30,146 +30,112 @@ object for tables
 */
 
 exports.showTrees = function(req, res, next) {
-    var neX = req.query.neLat,
-        neY = req.query.neLng,
-        swX = req.query.swLat,
-        swY = req.query.swLng;
-    // var bbox = JSON.parse(decodeURIComponent(encodedBBOX));
+  var neX = req.query.neLat,
+    neY = req.query.neLng,
+    swX = req.query.swLat,
+    swY = req.query.swLng,
+    filter = req.query.filter;
+  // var bbox = JSON.parse(decodeURIComponent(encodedBBOX));
 
 
-    //if the bounding box has three values come in on param...
-    if(neX && neY && swX && swY){
-        pg.connect(connstring, function(err, client, done) {
-            var handleError = function(err) {
-                if(!err) return false;
-                done(client);
-                next(err);
-                return true;
-            };
+  //if the bounding box has three values come in on param...
+  if (neX && neY && swX && swY) {
+    pg.connect(connstring, function(err, client, done) {
+      var handleError = function(err) {
+        if (!err) return false;
+        done(client);
+        next(err);
+        return true;
+      };
 
-                // if(!filter){
-                var myQuery = 'SELECT common_nam, genus, species, year, ST_AsGeoJSON(geom) AS geography ' +
-                    'FROM tree_plantingswgs84 ' +
-                    'WHERE ST_Intersects(geom, ST_GeometryFromText (\'POLYGON((' + swY + ' ' + swX + ',' + neY + ' ' + swX + ',' + neY + ' ' + neX + ',' + swY + ' ' + neX + ',' + swY + ' ' + swX + '))\', 4326 ));'
+      // if(!filter){
+      var myQuery = 'SELECT common_nam, genus, species, year, ST_AsGeoJSON(geom) AS geography ' +
+        'FROM tree_plantingswgs84 ' +
+        'WHERE ST_Intersects(geom, ST_GeometryFromText (\'POLYGON((' + swY + ' ' + swX + ',' + neY + ' ' + swX + ',' + neY + ' ' + neX + ',' + swY + ' ' + neX + ',' + swY + ' ' + swX + '))\', 4326 ));'
 
-                // var getStatus = 'SELECT count(gid) as total ' +
-                //         'FROM tree_plantingswgs84 ' +
-                //         'WHERE ST_Intersects(geom, ST_GeometryFromText (\'POLYGON((' + swY + ' ' + swX + ',' + neY + ' ' + swX + ',' + neY + ' ' + neX + ',' + swY + ' ' + neX + ',' + swY + ' ' + swX + '))\', 4326 )) ' +
-                //         'GROUP BY year;'
-                    // 'WHERE ST_Intersects(geom, ST_GeometryFromText (\'POLYGON((' + swX + ' ' + swY + ',' + neX + ' ' + swY + ',' + neX + ' ' + neY + ',' + swX + ' ' + neY + ',' + swX + ' ' + swY + '))\', 4326 ));'
-                // };
-                //with neighborhood filter
-                // if(filter){
-                //     console.log(filter);
-                //     var myQuery = 'SELECT common_nam, genus, species, ST_AsGeoJSON(geom) AS geography ' +
-                //         'FROM tree_plantingswgs84 ' +
-                //         'WHERE ST_Within(geom, (SELECT geom FROM neighborhoodwgs84 WHERE id = ' + filter + '));'
-                //     console.log(myQuery);
-                // }
-                // var featureCollection = new FeatureCollection();
-              // client.query(getStatus, function(err, result){
-              //   var sendOut = {}
-              //   console.log('1',err)
-              //   console.log('5',result)
-              //   if(result.rowCount) {
-              //     console.log('huh')
-              //     res.send(500);
-              //   }
-              //   else {
-              //     for (var a = 0; a < result.rowCount; a ++){
-              //       if(result.rows[a]){
-              //         sendOut[result.rows[a]] = sendOut[result.rows[a]] + total
-              //       }
-              //     }
-              //   }
-              //   console.log(sendOut.rows)
-              //   // featureCollection.properties[sendOut];
-              // })
-            client.query(myQuery, function(err, result) {
-                if(result.rowCount == 0) {
-                  res.send(500);
-                }
-                else {
-                  var featureCollection = new FeatureCollection();
-                  for(var i=0; i<result.rowCount; i++){
-                    var feature = new Feature();
-                    //feature.properties = ({"city_name":result.rows[i].city_name, "cntry_name":result.rows[i].cntry_name, "pop":result.rows[i].pop});
-                    feature.properties = ({
-                        "common_name":result.rows[i].common_nam,
-                        "genus":result.rows[i].genus,
-                        "species":result.rows[i].species,
-                        "year": parseInt(result.rows[i].year)
-                    })
-                    feature.geometry = JSON.parse(result.rows[i].geography);
-                    featureCollection.features.push(feature);
-                  }
-                  res.type('text/javascript');
-                  res.jsonp(featureCollection);
-                  done();
-                }
-            });
-        })
+      console.log(myQuery);
+      client.query(myQuery, function(err, result) {
+        if (result.rowCount == 0) {
+          res.send(500);
+        } else {
+          var featureCollection = new FeatureCollection();
+          for (var i = 0; i < result.rowCount; i++) {
+            var feature = new Feature();
+            //feature.properties = ({"city_name":result.rows[i].city_name, "cntry_name":result.rows[i].cntry_name, "pop":result.rows[i].pop});
+            feature.properties = ({
+              "common_name": result.rows[i].common_nam,
+              "genus": result.rows[i].genus,
+              "species": result.rows[i].species,
+              "year": parseInt(result.rows[i].year)
+            })
+            feature.geometry = JSON.parse(result.rows[i].geography);
+            featureCollection.features.push(feature);
+          }
+          res.type('text/javascript');
+          res.jsonp(featureCollection);
+          done();
+        }
+      });
+    })
 
-    }
-    else{
-        //send failuer
-    }
+  } else {
+    //send failuer
+  }
 }
 
 /*clustering experiments*/
 exports.clusterByReducedPrecision = function(req, res, next) {
   var precision = req.params.precision;
   var neX = req.query.neLat,
-      neY = req.query.neLng,
-      swX = req.query.swLat,
-      swY = req.query.swLng;
-    //if the bounding box has three values come in on param...
-        pg.connect(connstring, function(err, client, done) {
-            var handleError = function(err) {
-                if(!err) return false;
-                done(client);
-                next(err);
-                return true;
-            };
+    neY = req.query.neLng,
+    swX = req.query.swLat,
+    swY = req.query.swLng;
+  //if the bounding box has three values come in on param...
+  pg.connect(connstring, function(err, client, done) {
+    var handleError = function(err) {
+      if (!err) return false;
+      done(client);
+      next(err);
+      return true;
+    };
 
-                // if(!filter){
-              var  myQuery = 'SELECT count(gid) as total, \'{"type":"Point","coordinates":[\'||round(longitude, '+precision+')||\',\'||round(latitude, '+precision+')||\']}\' as geography ' +
-                             'FROM tree_plantingswgs84 ' +
-                             'WHERE ST_Intersects(geom, ST_GeometryFromText (\'POLYGON((' + swY + ' ' + swX + ',' + neY + ' ' + swX + ',' + neY + ' ' + neX + ',' + swY + ' ' + neX + ',' + swY + ' ' + swX + '))\', 4326 )) ' +
-                             'GROUP BY round(latitude, '+precision+'), round(longitude, '+precision+');'
+    // if(!filter){
+    var myQuery = 'SELECT count(gid) as total, \'{"type":"Point","coordinates":[\'||round(longitude, ' + precision + ')||\',\'||round(latitude, ' + precision + ')||\']}\' as geography ' +
+      'FROM tree_plantingswgs84 ' +
+      'WHERE ST_Intersects(geom, ST_GeometryFromText (\'POLYGON((' + swY + ' ' + swX + ',' + neY + ' ' + swX + ',' + neY + ' ' + neX + ',' + swY + ' ' + neX + ',' + swY + ' ' + swX + '))\', 4326 )) ' +
+      'GROUP BY round(latitude, ' + precision + '), round(longitude, ' + precision + ');'
 
-                //with neighborhood filter
-                // if(filter){
-                //     console.log(filter);
-                //     var myQuery = 'SELECT common_nam, genus, species, ST_AsGeoJSON(geom) AS geography ' +
-                //         'FROM tree_plantingswgs84 ' +
-                //         'WHERE ST_Within(geom, (SELECT geom FROM neighborhoodwgs84 WHERE id = ' + filter + '));'
-                //     console.log(myQuery);
-                // }
+    //with neighborhood filter
+    // if(filter){
+    //     console.log(filter);
+    //     var myQuery = 'SELECT common_nam, genus, species, ST_AsGeoJSON(geom) AS geography ' +
+    //         'FROM tree_plantingswgs84 ' +
+    //         'WHERE ST_Within(geom, (SELECT geom FROM neighborhoodwgs84 WHERE id = ' + filter + '));'
+    //     console.log(myQuery);
+    // }
 
-            client.query(myQuery, function(err, result) {
-              var totalTrees = 0;
-                if(result.rowCount == 0) {
-                  res.send(500);
-                }
-                else {
-                  var featureCollection = new FeatureCollection();
-                  for(var i=0; i<result.rowCount; i++){
-                    totalTrees = totalTrees + parseInt(result.rows[i].total);
-                    var feature = new Feature();
-                    //feature.properties = ({"city_name":result.rows[i].city_name, "cntry_name":result.rows[i].cntry_name, "pop":result.rows[i].pop});
-                    feature.properties = ({
-                        "count":result.rows[i].total
-                    })
-                    feature.geometry = JSON.parse(result.rows[i].geography);
-                    featureCollection.features.push(feature);
-                  }
-                  featureCollection.total = totalTrees;
-                  res.type('text/javascript');
-                  res.jsonp(featureCollection);
-                  done();
-                }
-            });
-        })
+    client.query(myQuery, function(err, result) {
+      var totalTrees = 0;
+      if (result.rowCount == 0) {
+        res.send(500);
+      } else {
+        var featureCollection = new FeatureCollection();
+        for (var i = 0; i < result.rowCount; i++) {
+          totalTrees = totalTrees + parseInt(result.rows[i].total);
+          var feature = new Feature();
+          //feature.properties = ({"city_name":result.rows[i].city_name, "cntry_name":result.rows[i].cntry_name, "pop":result.rows[i].pop});
+          feature.properties = ({
+            "count": result.rows[i].total
+          })
+          feature.geometry = JSON.parse(result.rows[i].geography);
+          featureCollection.features.push(feature);
+        }
+        featureCollection.total = totalTrees;
+        res.type('text/javascript');
+        res.jsonp(featureCollection);
+        done();
+      }
+    });
+  })
 
 }
